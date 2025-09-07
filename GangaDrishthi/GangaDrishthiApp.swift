983592ec7c6fa492014321e4ec86a21cb1a8a7c3
YaduLuxe <yadurajsingham@gmail.com>
2025-09-07 14:29:32 +0530

    first commit

---
+++ GangaDrishthi/GangaDrishthiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GangaDrishthiApp.swift
+//  GangaDrishthi
+//
+//  Created by Yaduraj Singh on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GangaDrishthiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GangaDrishthiTests/GangaDrishthiTests.swift
@@ -0,0 +1,17 @@
+//
+//  GangaDrishthiTests.swift
+//  GangaDrishthiTests
+//
+//  Created by Yaduraj Singh on 07/09/25.
+//
+
+import Testing
+@testable import GangaDrishthi
+
+struct GangaDrishthiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
